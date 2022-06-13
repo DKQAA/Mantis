15047006544e1ca594c9bd9656d0c1b90a00de3c
DKQAA <dima02ufa@yandex.ru>
2022-06-13 16:06:16 +0500

    Лекция 9.1

---
+++ mantis-tests/mantis-tests/appmanager/RegistrationHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+
+namespace mantis_tests
+{
+    public class RegistrationHelper : HelperBase
+    {
+        public RegistrationHelper(ApplicationManager manager) : base(manager){}
+
+        public void Register(AccountData account)
+        {
+            OpenMainPage();
+            OpenRegistrationForm();
+            FillRegistrationForm(account);
+            SubmitRegistration();
+        }
+       
+
+        void OpenRegistrationForm()
+        {
+            //driver.FindElement(By.LinkText("Зарегистрировать новую учётную запись")).Click();
+            driver.FindElement(By.XPath("//a[contains(@href, 'signup_page.php')]")).Click();
+
+        }
+
+        void SubmitRegistration()
+        {
+            driver.FindElement(By.XPath("//*[@type='submit']")).Click();
+        }
+
+        void FillRegistrationForm(AccountData account)
+        {
+            driver.FindElement(By.Name("username")).SendKeys(account.Name);
+            driver.FindElement(By.Name("email")).SendKeys(account.Email);
+        }
+
+        void OpenMainPage()
+        {
+            manager.Driver.Url = "http://localhost/mantisbt-2.25.4/login_page.php";
+        }
+    }
+}
---
+++ mantis-tests/mantis-tests/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = '668MkJ0SUHYxIhJBIK8nL2EPc0yf7D8TQJiD801MViA=';
+$g_signup_use_captcha     = OFF;
